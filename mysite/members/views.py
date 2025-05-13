b9ed76ae369cd563c7ab7c91eeb1f230562c325f
NguyenDuyCuong <cuongnd.ami@outlook.com>
2025-05-13 01:54:13 +0000

    some template html files

--- mysite/members/views.py
+++ mysite/members/views.py
@@ -27,8 +27,8 @@ def main(request):
 
 
 def testing(request):
-    template = loader.get_template('template.html')
-    context = {
-        'fruits': ['Apple', 'Banana', 'Cherry'],
-    }
-    return HttpResponse(template.render(context, request))
+  template = loader.get_template('template.html')
+  context = {
+    'firstname': 'Linus',
+  }
+  return HttpResponse(template.render(context, request))
